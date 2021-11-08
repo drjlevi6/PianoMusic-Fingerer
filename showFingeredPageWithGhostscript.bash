95b6796283c0715107c5d98931f0ba2993e14e2c
drjlevi6 <drjlevi6@aol.com>
2021-11-08 08:43:52 -0500

    Create showFingeredPageWithGhostscript.bash
    Shell script to draw fingered piano music with GhostScript

---
+++ showFingeredPageWithGhostscript.bash
@@ -0,0 +1,7 @@
+#! /opt/local/bin/bash
+
+# Uses GhostScript to draw the page of piano-music represented by
+# f8-fromFile-p1.ps, with additional definitions provided by dimensions.ps, 
+# vshow.ps, and user-supplied fingerings in fingerings.ps.
+
+gs -dNOSAFER dimensions.ps f8-fromFile-p1.ps vshow.ps fingerings.ps
