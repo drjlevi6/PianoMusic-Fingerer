2635c0c419da5105bda87fbe4fad4747facd6f3e
drjlevi6 <drjlevi6@aol.com>
2021-11-08 10:47:13 -0500

    Added pixels.txt; updated showFingeredPageWithGhostscript.bash
    pixels.txt provides the data to draw the original page, to which the fingerings are added.

--- showFingeredPageWithGhostscript.bash
+++ showFingeredPageWithGhostscript.bash
@@ -4,4 +4,4 @@
 # f8-fromFile-p1.ps, with additional definitions provided by dimensions.ps, 
 # vshow.ps, and user-supplied fingerings in fingerings.ps.
 
-gs -dNOSAFER dimensions.ps f8-fromFile-p1.ps vshow.ps fingerings.ps
+gs -dNOSAFER dimensions.ps f8-fromFile-page1.ps vshow.ps fingerings.ps
